18c9ba4ec4b1d1d06ed75a09304e6c792cb3e1fc
Hanna Lindgren <han.lind@gmail.com>
2017-04-28 17:00:11 +0200

    doc

--- doc.go
+++ doc.go
@@ -1,6 +1,7 @@
 /*Package rbg2p contains utilities for rule based, manually written, grapheme to phoneme rules.
 
 Each g2p rule set is defined in a .g2p file with the following content:
+
     * specific variables - used to define constant variables such as character set and phoneme delimiter
     * variables (optional) - any variables for use in the context of the actual rules
     * rules - g2p rules
