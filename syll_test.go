5e3a24e4360ed56992fe8088e9605a911abf5246
Hanna Lindgren <hanna@stts.se>
2021-05-06 09:07:00 +0200

    syll_test.go

--- syll_test.go
+++ syll_test.go
@@ -521,6 +521,66 @@ func TestSwsInputWithStressPlacement_FirstInSyllable(t *testing.T) {
 
 }
 
+func TestStressPlacements(t *testing.T) {
+	var fsExpGot = "Input: %s; Expected: %v got: %v"
+
+	var err error
+	var def SyllDef
+	var stressP StressPlacement
+	var expect, result string
+	var input []string
+
+	var baseLines = []string{"SYLLDEF TYPE MOP",
+		`SYLLDEF ONSETS "r, t, p, s, d, f, g, h, j, k, l, v, b, n, m, p r"`,
+		`SYLLDEF SYLLABIC "a o u e i"`,
+		`SYLLDEF STRESS "1"`,
+		`SYLLDEF DELIMITER "."`,
+	}
+	def, stressP, err = loadSyllDef(append(baseLines, "SYLLDEF STRESS_PLACEMENT AfterSyllabic"), " ")
+	if err != nil {
+		t.Errorf("%v", err)
+		return
+	}
+	syllAfterSyllabic := Syllabifier{SyllDef: def, StressPlacement: stressP}
+
+	def, stressP, err = loadSyllDef(append(baseLines, "SYLLDEF STRESS_PLACEMENT BeforeSyllabic"), " ")
+	if err != nil {
+		t.Errorf("%v", err)
+		return
+	}
+	syllBeforeSyllabic := Syllabifier{SyllDef: def, StressPlacement: stressP}
+
+	def, stressP, err = loadSyllDef(append(baseLines, "SYLLDEF STRESS_PLACEMENT FirstInSyllable"), " ")
+	if err != nil {
+		t.Errorf("%v", err)
+		return
+	}
+	syllFirstInSyllable := Syllabifier{SyllDef: def, StressPlacement: stressP}
+
+	//
+
+	input = strings.Split("d u 1 k a", " ")
+	expect = "d u 1 . k a"
+	result = syllAfterSyllabic.SyllabifyFromPhonemes(input)
+	if result != expect {
+		t.Errorf(fsExpGot, input, expect, result)
+	}
+
+	input = strings.Split("d 1 u k a", " ")
+	expect = "d 1 u . k a"
+	result = syllBeforeSyllabic.SyllabifyFromPhonemes(input)
+	if result != expect {
+		t.Errorf(fsExpGot, input, expect, result)
+	}
+
+	input = strings.Split("d 1 u k a", " ")
+	expect = "1 d u . k a"
+	result = syllFirstInSyllable.SyllabifyFromPhonemes(input)
+	if result != expect {
+		t.Errorf(fsExpGot, input, expect, result)
+	}
+}
+
 func TestSwsInputWithStressPlacement_AfterSyllabic(t *testing.T) {
 	var fsExpGot = "Input: %s; Expected: %v got: %v"
 
